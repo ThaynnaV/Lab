f2d3953f1557bedb51511db693b4499b1abb09ab
xochitl-b <xochitl.burtnik@gmail.com>
2023-12-15 13:38:49 +0000

    setting up the displayed menu

--- AADP_Lab_SoccerSimulator/src/aadp_lab_soccersimulator/Menu.java
+++ AADP_Lab_SoccerSimulator/src/aadp_lab_soccersimulator/Menu.java
@@ -4,10 +4,31 @@
  */
 package aadp_lab_soccersimulator;
 
+import java.util.Scanner;
+
 /**
  *
  * @author lafla
  */
 public class Menu {
+    Scanner sc = new Scanner(System.in);
+    private int option = 0;
+    private boolean isValidOption = false;
     
+    public Menu() {
+    
+    }
+    public void DisplayMenuAndGetOption(){
+        //first show options to the  user
+        do {
+            System.out.println("Welcome! What would you like to do today? Please enter the number corresponding to your choice.");
+            System.out.println("1. Enter a new player to a team.");
+            System.out.println("2. See the players on a team.");
+            System.out.println("3. Simulate a number of matches.");
+            System.out.println("4. Exit the programme.");            
+        
+        
+        } while (!isValidOption);//whie the options are correct
+        
+    }    
 }
